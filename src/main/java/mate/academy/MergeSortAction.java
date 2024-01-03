5fa45fe19430fbb984452fd42fdf7cda2f73ca0e
denysSmeiNewBom <den.smei.ds@gmail.com>
2024-01-03 23:39:17 +0200

    Task completed

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,42 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+    private final int[] array;
 
     public MergeSortAction(int[] array) {
+        this.array = array;
+    }
 
+    @Override
+    protected void compute() {
+        if (array.length < 2) {
+            return;
+        }
+        var leftArray = Arrays.copyOfRange(array, 0, array.length / 2);
+        var rightArray = Arrays.copyOfRange(array, array.length / 2, array.length);
+        invokeAll(new MergeSortAction(leftArray), new MergeSortAction(rightArray));
+        merge(leftArray, rightArray);
+    }
+
+    private void merge(int[] leftArray, int[] rightArray) {
+        int i = 0;
+        int j = 0;
+        while (i < leftArray.length && j < rightArray.length) {
+            if (leftArray[i] < rightArray[j]) {
+                array[i + j] = leftArray[i++];
+            } else {
+                array[i + j] = rightArray[j++];
+            }
+        }
+
+        while (i < leftArray.length) {
+            array[i + j] = leftArray[i++];
+        }
+        while (j < rightArray.length) {
+            array[i + j] = rightArray[j++];
+        }
     }
 }
