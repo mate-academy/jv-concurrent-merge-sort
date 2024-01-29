29c41038ae706a0da4504b4215517586172db190
Aleksandr Zahorodnii <86927716+0-zagar-0@users.noreply.github.com>
2024-01-29 21:30:31 +0100

    implemented solution

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,32 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+    private static final int THRESHOLD = 3;
+    private final int[] array;
+    private final int start;
+    private final int end;
 
     public MergeSortAction(int[] array) {
+        this(array, 0, array.length);
+    }
 
+    public MergeSortAction(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if (end - start <= THRESHOLD) {
+            Arrays.sort(array);
+        } else {
+            int middle = start + (end - start) / 2;
+            new MergeSortAction(array, start, middle).fork().join();
+            new MergeSortAction(array, middle, end).fork().join();
+        }
     }
 }
