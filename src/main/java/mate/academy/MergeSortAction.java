59f13e8df2b1e54337262255eb549afe9b16085c
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-04 15:13:26 +0200

    Added MergeSortAction impl.

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,55 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+    private static final int THRESHOLD = 2;
+    private final int[] array;
 
     public MergeSortAction(int[] array) {
+        this.array = array;
+    }
 
+    @Override
+    protected void compute() {
+        if (array.length < THRESHOLD) {
+            Arrays.sort(array);
+        } else {
+
+            int mid = array.length / 2;
+
+            int[] left = Arrays.copyOfRange(array, 0, mid);
+            int[] right = Arrays.copyOfRange(array, mid, array.length);
+
+            MergeSortAction leftTask = new MergeSortAction(left);
+            MergeSortAction rightTask = new MergeSortAction(right);
+
+            invokeAll(leftTask, rightTask);
+
+            merge(array, left, right);
+        }
+    }
+
+    private void merge(int[] array, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
     }
 }
