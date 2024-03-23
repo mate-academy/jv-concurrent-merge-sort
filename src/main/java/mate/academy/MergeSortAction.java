a43202a4190f60d77e10f6b6aae9376a64866cb2
Vitalii <vitaliyplys1@gmail.com>
2024-03-23 20:42:34 +0200

    Add fixes

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -22,11 +22,35 @@ public class MergeSortAction extends RecursiveAction {
     @Override
     protected void compute() {
         if (end - start <= THRESHOLD) {
-            Arrays.sort(array);
+            Arrays.sort(array, start, end);
         } else {
             int middle = start + (end - start) / 2;
-            new MergeSortAction(array, start, middle).fork().join();
-            new MergeSortAction(array, middle, end).fork().join();
+            MergeSortAction leftAction = new MergeSortAction(array, start, middle);
+            MergeSortAction rightAction = new MergeSortAction(array, middle, end);
+            invokeAll(leftAction, rightAction);
+            merge(array, start, middle, end);
         }
+    }
+
+    private void merge(int[] array, int start, int middle, int end) {
+        int[] temp = new int[end - start];
+        int i = start;
+        int j = middle;
+        int k = 0;
+
+        while (i < middle && j < end) {
+            if (array[i] <= array[j]) {
+                temp[k++] = array[i++];
+            } else {
+                temp[k++] = array[j++];
+            }
+        }
+        while (i < middle) {
+            temp[k++] = array[i++];
+        }
+        while (j < end) {
+            temp[k++] = array[j++];
+        }
+        System.arraycopy(temp, 0, array, start, temp.length);
     }
 }
