80f1fadeba3155d83ff0bb81a4583632fed08dfb
MateuszRostek <17rostek17@gmail.com>
2024-04-10 01:06:12 +0200

    Refactored code to use subarray as a recursive param

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -4,43 +4,32 @@ import java.util.concurrent.RecursiveAction;
 
 public class MergeSortAction extends RecursiveAction {
     private static final int THRESHOLD = 1;
-    private int start;
-    private int end;
     private int[] array;
 
     public MergeSortAction(int[] array) {
-        this(0, array.length, array);
-    }
-
-    private MergeSortAction(int start, int end, int[] array) {
-        this.start = start;
-        this.end = end;
         this.array = array;
     }
 
     @Override
     protected void compute() {
-        if ((end - start) > THRESHOLD) {
-            int middle = start + (end - start) / 2;
-            MergeSortAction leftSubTask = new MergeSortAction(start, middle, array);
-            MergeSortAction rightSubTask = new MergeSortAction(middle, end, array);
-            leftSubTask.fork();
-            rightSubTask.fork();
-            leftSubTask.join();
-            rightSubTask.join();
-            sortAndMerge(start, end);
+        if (array.length > THRESHOLD) {
+            int middle = array.length / 2;
+            int[] leftArray = new int[middle];
+            int[] rightArray = new int[array.length - middle];
+            System.arraycopy(array, 0, leftArray, 0, leftArray.length);
+            System.arraycopy(array, middle, rightArray, 0, rightArray.length);
+            MergeSortAction leftSubTask = new MergeSortAction(leftArray);
+            MergeSortAction rightSubTask = new MergeSortAction(rightArray);
+            leftSubTask.fork().join();
+            rightSubTask.fork().join();
+            merge(array, leftArray, rightArray);
         }
     }
 
-    private void sortAndMerge(int start, int end) {
-        int middle = start + (end - start) / 2;
-        int[] leftArray = new int[middle - start];
-        int[] rightArray = new int[end - middle];
-        System.arraycopy(array, start, leftArray, 0, leftArray.length);
-        System.arraycopy(array, middle, rightArray, 0, rightArray.length);
+    private void merge(int[] array, int[] leftArray, int[] rightArray) {
         int leftIndex = 0;
         int rightIndex = 0;
-        int mainArrayIndex = start;
+        int mainArrayIndex = 0;
 
         while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
             if (leftArray[leftIndex] <= rightArray[rightIndex]) {
