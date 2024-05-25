58b67ed7f37e93fe1168b1f712f6dd9a3f244adc
Kacper <kacperkukula97@gmail.com>
2024-05-25 16:04:20 +0200

    Implemented Recursive Action - Merge Sort

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,61 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+    private static final int THRESHOLD = 2;
+
+    private final int[] array;
+    private final int start;
+    private final int end;
 
     public MergeSortAction(int[] array) {
+        this(array, 0, array.length);
+    }
 
+    private MergeSortAction(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if (end - start <= THRESHOLD) {
+            Arrays.sort(array, start, end);
+        } else {
+            int middle = (start + end) / 2;
+            MergeSortAction left = new MergeSortAction(array, start, middle);
+            MergeSortAction right = new MergeSortAction(array, middle, end);
+            invokeAll(left, right);
+
+            mergeArrays(start, middle, end);
+        }
+    }
+
+    private void mergeArrays(int start, int middle, int end) {
+        int[] mergedArray = new int[end - start];
+        int left = start;
+        int right = middle;
+        int mergeIndex = 0;
+
+        while (left < middle && right < end) {
+            if (array[left] <= array[right]) {
+                mergedArray[mergeIndex++] = array[left++];
+            } else {
+                mergedArray[mergeIndex++] = array[right++];
+            }
+        }
+
+        while (left < middle) {
+            mergedArray[mergeIndex++] = array[left++];
+        }
+
+        while (right < end) {
+            mergedArray[mergeIndex++] = array[right++];
+        }
+
+        System.arraycopy(mergedArray, 0, array, start, mergedArray.length);
     }
 }
