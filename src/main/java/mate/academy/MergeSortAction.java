415a36df5716bb6e3fd828770a61030ff9c36e43
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-28 00:03:06 +0300

    added task

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -6,13 +6,9 @@ import java.util.concurrent.RecursiveAction;
 
 public class MergeSortAction extends RecursiveAction {
     private static final int THRESHOLD = 2;
-    private int[] ints;
+    private final int[] ints;
     private final int from;
     private final int to;
-
-    public int[] getInts() {
-        return ints;
-    }
 
     private MergeSortAction(int[] massiv, int from, int to) {
         this.ints = massiv;
@@ -26,41 +22,20 @@ public class MergeSortAction extends RecursiveAction {
 
     @Override
     protected void compute() {
-        if (ints.length >= THRESHOLD) {
-            MergeSortAction leftAction = new MergeSortAction(Arrays.copyOfRange(ints, from, ints.length / 2));
-            MergeSortAction rightAction = new MergeSortAction(Arrays.copyOfRange(ints, ints.length / 2, to));
-            invokeAll(leftAction, rightAction);
-            merge(leftAction.getInts(), rightAction.getInts());
-        }
-    }
-
-    private void merge(int[] left, int[] right) {
-        int i = 0;
-        int j = 0;
-        while (i < left.length && j < right.length) {
-            if (left[i] < right[j]) {
-                ints[i + j] = left[i];
-                i++;
-            } else {
-                ints[i + j] = right[j];
-                j++;
-            }
-        }
-
-        while (i < left.length) {
-            this.ints[i + j] = left[i];
-            i++;
-        }
-        while (j < right.length) {
-            this.ints[i + j] = right[j];
-            j++;
+        if (to - from >= THRESHOLD) {
+            int middle = (from + to) >>> 1;
+            invokeAll(new MergeSortAction(ints, from, middle),
+                      new MergeSortAction(ints, middle, to));
+            merge(from, middle, to);
+        } else {
+            Arrays.sort(ints, from, to);
         }
     }
 
     private void merge(int from, int middle, int to) {
-        int[] sortedMassiv = Arrays.copyOfRange(this.ints, from, middle);
-        for (int i = 0, j = from, k = middle; i < sortedMassiv.length; j++) {
-            this.ints[j] = (k == to || sortedMassiv[i] < this.ints[k]) ? sortedMassiv[i++] : this.ints[k++];
+        int[] copyInts = Arrays.copyOfRange(ints, from, middle);
+        for (int i = 0, j = from, k = middle; i < copyInts.length; j++) {
+            ints[j] = (k == to || copyInts[i] < ints[k]) ? copyInts[i++] : ints[k++];
         }
     }
 }
