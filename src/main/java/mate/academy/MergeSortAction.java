e97a60c49cf852d0346848d126f88d93269ddc00
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-28 10:40:39 +0300

    fixed after review

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -5,36 +5,42 @@ import java.util.concurrent.RecursiveAction;
 
 public class MergeSortAction extends RecursiveAction {
     private static final int THRESHOLD = 2;
-    private final int[] ints;
-    private final int from;
-    private final int to;
+    private final int[] array;
+    private final int start;
+    private final int end;
 
-    private MergeSortAction(int[] massiv, int from, int to) {
-        this.ints = massiv;
-        this.from = from;
-        this.to = to;
+    public MergeSortAction(int[] array) {
+        this(array, 0, array.length);
     }
 
-    public MergeSortAction(int[] massiv) {
-        this(massiv, 0, massiv.length);
+    private MergeSortAction(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
     }
 
     @Override
     protected void compute() {
-        if (to - from >= THRESHOLD) {
-            int middle = (from + to) >>> 1;
-            invokeAll(new MergeSortAction(ints, from, middle),
-                      new MergeSortAction(ints, middle, to));
-            merge(from, middle, to);
+        if (end - start >= THRESHOLD) {
+            int middle = (start + end) >>> 1;
+            invokeAll(new MergeSortAction(array, start, middle),
+                      new MergeSortAction(array, middle, end));
+            merge(start, middle, end);
         } else {
-            Arrays.sort(ints, from, to);
+            Arrays.sort(array, start, end);
         }
     }
 
-    private void merge(int from, int middle, int to) {
-        int[] copyInts = Arrays.copyOfRange(ints, from, middle);
-        for (int i = 0, j = from, k = middle; i < copyInts.length; j++) {
-            ints[j] = (k == to || copyInts[i] < ints[k]) ? copyInts[i++] : ints[k++];
+    private void merge(int start, int middle, int end) {
+        int[] buffer = Arrays.copyOfRange(array, start, middle);
+        for (int i = start, leftIndex = 0, rightIndex = middle; leftIndex < buffer.length; i++) {
+            if (rightIndex == end || buffer[leftIndex] < array[rightIndex]) {
+                array[i] = buffer[leftIndex];
+                leftIndex++;
+            } else {
+                array[i] = array[rightIndex];
+                rightIndex++;
+            }
         }
     }
 }
