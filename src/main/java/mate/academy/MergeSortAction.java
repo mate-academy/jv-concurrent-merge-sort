d5aa9ef2bab9e2e2738263ba853b8edb06893640
Oleksii Kolinko <138433107+OleksiiKolinko@users.noreply.github.com>
2024-08-22 00:45:45 +0200

    HW62-solution

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,50 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+    private static final int TWO = 2;
+    private static final int ZERO = 0;
+    private final int[] array;
 
     public MergeSortAction(int[] array) {
+        this.array = array;
+    }
 
+    @Override
+    protected void compute() {
+        if (array.length <= TWO) {
+            Arrays.sort(array);
+        } else {
+            int midl = array.length / TWO;
+            MergeSortAction firstPart = new MergeSortAction(Arrays.copyOfRange(array, ZERO, midl));
+            MergeSortAction secondPart =
+                    new MergeSortAction(Arrays.copyOfRange(array, midl, array.length));
+            firstPart.fork();
+            secondPart.fork();
+            firstPart.join();
+            secondPart.join();
+            merge(firstPart.array, secondPart.array);
+        }
+    }
+
+    private void merge(int[] firstArray, int[] secondArray) {
+        int i = ZERO;
+        int j = ZERO;
+        int a = ZERO;
+        while (i < firstArray.length && j < secondArray.length) {
+            if (firstArray[i] < secondArray[j]) {
+                array[a++] = firstArray[i++];
+            } else {
+                array[a++] = secondArray[j++];
+            }
+        }
+        while (i < firstArray.length) {
+            array[a++] = firstArray[i++];
+        }
+        while (j < secondArray.length) {
+            array[a++] = secondArray[j++];
+        }
     }
 }
