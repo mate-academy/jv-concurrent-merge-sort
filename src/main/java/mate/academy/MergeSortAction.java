68a4e344a81ad544faf45d1a4dc00bf79bc67715
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2025-02-10 14:04:12 +0100

    Done Concurrent Merge Sort. Attempt 1.0 - implemented the Merge Sort algorithm;

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,39 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+    private final int[] array;
 
     public MergeSortAction(int[] array) {
+        this.array = array;
+    }
 
+    @Override
+    protected void compute() {
+        if (array.length == 0) {
+            List<RecursiveAction> actions = new ArrayList<>(createSubTasks());
+            for (RecursiveAction action : actions) {
+                action.fork();
+            }
+        } else {
+            Arrays.sort(array);
+        }
+    }
+
+    private List<RecursiveAction> createSubTasks() {
+        List<RecursiveAction> subTasks = new ArrayList<>();
+        MergeSortAction first = new MergeSortAction(
+                Arrays.copyOfRange(array, 0, array.length / 2));
+        MergeSortAction second = new MergeSortAction(
+                Arrays.copyOfRange(array, array.length / 2, array.length));
+
+        subTasks.add(first);
+        subTasks.add(second);
+
+        return subTasks;
     }
 }
